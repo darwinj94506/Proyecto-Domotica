b60ead0bcff9e4f08335c10d73852a84c379822d
Darwin Guailla <darwinj94@hotmail.com>
2018-06-05 23:30:11 -0500

    funciona el canvas con el fondo

--- src/app/layout/bs-element/bs-element.module.ts
+++ src/app/layout/bs-element/bs-element.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ColorPickerModule } from 'ngx-color-picker';
+
 
 import { BsElementRoutingModule } from './bs-element-routing.module';
 import { BsElementComponent } from './bs-element.component';
 import { PageHeaderModule } from './../../shared';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
-    imports: [CommonModule, BsElementRoutingModule, PageHeaderModule],
+    imports: [CommonModule,
+        // ColorPickerModule,
+        FormsModule,
+        ColorPickerModule,
+        NgbModule.forRoot(),
+         BsElementRoutingModule, PageHeaderModule],
     declarations: [BsElementComponent]
 })
 export class BsElementModule {}
